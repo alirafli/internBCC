82fa6948b6f94c949f62b7547957f7e9d50cc31e
Mohammad Ali Rafli <mrafli1212@gmail.com>
2021-03-02 20:07:30 +0700

    edit foodlist to responsive

--- src/component/foodList/StyleFoodList.js
+++ src/component/foodList/StyleFoodList.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 
 export const Container = styled.div`
   margin-top: 3.2rem;
-  
 `;
 
 export const Text = styled.p`
@@ -11,17 +10,19 @@ export const Text = styled.p`
   color: rgba(34, 34, 34, 0.7);
   font-weight: normal;
   font-size: 1.4rem;
-  line-height: 1.6rem;
+  line-height: 1.4rem;
   letter-spacing: 0.03em;
-  margin:1.5rem;
-  text-align: center;
+  margin-right: 3.4rem;
+  padding: .5rem;
 `;
 
 export const Wrapper = styled.div`
   display: flex;
-  justify-content: space-around;
   direction: row;
-  justify: center;
+  justify-content: center;
   margin-bottom: 2.3rem; 
   flex-wrap: wrap;
+  @media only screen and (max-width:63.3rem){
+    
+} 
 `;
